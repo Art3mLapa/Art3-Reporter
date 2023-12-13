4b1328f37fc2b6cceea36f4bc16128315344ae48
Art3_D3v <76118653+Art3mLapa@users.noreply.github.com>
2023-12-13 07:55:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ links = [
     "https://www.roblox.com/abuseReport/group?id=",
     "https://www.roblox.com/abusereport/userprofile?id="
 ]
-
-# Проверьте, что введенное значение в диапазоне 1-3
 selected_link = links[link_choice - 1] if 1 <= link_choice <= 3 else None
 
 print("\033[33m[1] Inappropriate Language - Profanity & Adult Content")
@@ -38,20 +36,16 @@ print("[7] Inappropriate Content - Place, Image, Model")
 print("[8] Real Life Threats & Suicide Threats\033[0m")
 
 reason_choice = input("\033[31m[?] Choice reason for report(1/.../8): ")
-
 victim_id = input("\033[31m[?] Enter the victim's user/asset id: ")
-
 script_directory = os.path.dirname(os.path.abspath(__file__))
-
 chrome_driver_path = os.path.join(script_directory, 'chromedriver.exe')
-
 chrome_service = ChromeService(chrome_driver_path)
 
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-extensions")
 options.add_argument("--disable-notifications")
-#options.add_argument("--headless")
-#options.add_argument("--log-level=3")
+options.add_argument("--headless")
+options.add_argument("--log-level=3")
 
 browser = webdriver.Chrome(service=chrome_service, options=options)
 
